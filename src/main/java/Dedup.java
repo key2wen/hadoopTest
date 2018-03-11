317cb56953e6571e03c235f84dd6765d3bc3a35f
shuguang <shuguang@tongbanjie.com>
2018-03-11 17:12:08 +0800

    x

--- src/main/java/Dedup.java
+++ src/main/java/Dedup.java
@@ -42,6 +42,8 @@ public class Dedup {
 
     public static void main(String[] args) throws Exception {
 
+        //D:/git_work/hadoopTest/input D:/git_work/hadoopTest/output
+
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
